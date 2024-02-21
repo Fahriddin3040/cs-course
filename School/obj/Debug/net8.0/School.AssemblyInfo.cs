48f2096dbb521a5b7b0ff8e9b9fbb2088b753dd1
Fakhriddin <fahriddin3040@gmail.com>
2024-02-21 17:28:34 +0500

    Services and gits

--- School/obj/Debug/net8.0/School.AssemblyInfo.cs
+++ School/obj/Debug/net8.0/School.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7afe38a6a0d7d12c09f69ae7b76f84c20d4645d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18bcb87f86b53850f5580c5310b9130fdafc89d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("School")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
